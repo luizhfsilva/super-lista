36f656b8b7c3a4f1df1b3691c6c81c9b042fa41e
Luiz Silva <luizhfsilva@gmail.com>
2021-02-27 13:02:58 -0300

    heroku

--- run.ps1
+++ run.ps1
@@ -1,5 +1,6 @@
 $env:PGUSER = "postgres"
 $env:PGPASSWORD = "secret"
 $env:PGDATABASE = "postgres"
+$env:DATABASE_URL = "postgres://${PGUSER}:${PGPASSWORD}@127.0.0.1:5432/${PGDATABASE}"
 
 node .\app.js
